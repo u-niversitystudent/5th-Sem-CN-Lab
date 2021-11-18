af8456404e3fb4761f5d9f7998110aba477394eb
Mɪᴇɢʀᴀᴛ Sᴀᴍᴍʀɪ <subhojitg@live.com>
2021-11-18 15:41:28 +0545

    Update 7_remoteServer.c

--- 7_remoteServer.c
+++ 7_remoteServer.c
@@ -4,6 +4,12 @@ Q.7. Program for “Remote Command Execution” using sockets. (Description: The
 
 // SERVER
 
+/*
+Q.7. Program for “Remote Command Execution” using sockets. (Description: The client sends command(s) and data parameters (if required) to the server. The server will execute the command and send back the result to the client. Execute 3 commands.)
+*/
+
+// SERVER
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -14,13 +20,16 @@ Q.7. Program for “Remote Command Execution” using sockets. (Description: The
 #include <sys/stat.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <unistd.h>
 
 #define MAX 1000
 
 int main() {
 	int serverDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
     int size;
-    char buffer[MAX], message[] = "Command Successfully executed !";
+    char buffer[MAX], message[10][MAX], msg[MAX]=" ";
+	char out[]=">a.txt";
+	
     struct sockaddr_in clientAddress, serverAddress;
 
     socklen_t clientLength = sizeof(clientAddress);
@@ -30,18 +39,28 @@ int main() {
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddress.sin_port = htons(9976);
 
+	int length;
     bind(serverDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
     while (1) {
         bzero(buffer, sizeof(buffer));
+		bzero(msg,1000);
         recvfrom(serverDescriptor, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientAddress, &clientLength);
-        system(buffer);
+		system(buffer);
         printf("Command Executed ... %s ", buffer);
-        sendto(serverDescriptor, message, sizeof(message), 0, (struct sockaddr *)&clientAddress, clientLength);
+        length=strlen(buffer) - 1;
+		printf ("%d", length);
+		buffer[length]='>';
+		buffer[length+1]='a';
+		buffer[length+2]='\0';
+		system(buffer);
+		FILE *file = fopen ("a", "r");
+		for (int i=0; i<10; i++) {
+			fgets(message[i], 50, file);
+			strcat(msg, message[i]);
+			bzero(message[i],50);
+		}
+		sendto(serverDescriptor, msg, sizeof(msg), 0, (struct sockaddr *)&clientAddress, clientLength);
     }
     close(serverDescriptor);
     return 0;
 }
-
-// As noted, we are required to print the output on the client terminal, and not the server terminal. To do this, there is no solution, as far as I know.
-// So. just for the sake of lab output, it is suggested that you use system (buffer) in the client program and take screenshot.
-// During viva/presentation also, prepare a fake client side program that uses system (buffer) to show TAs that the results are printed on client terminal.
